197265c252ca82d4fa1318aa44e282d7efbb6553
DALAYAI TEJA <107502862+teja96764247@users.noreply.github.com>
2023-09-29 23:28:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 diabetics_model=pickle.load(open('https://github.com/teja96764247/MULTIPLE-DISEASE-PREDICTIVE-SYSTEM./blob/main/diabetic_model.sav','rb'))
 
 
-parkinsson_model=pickle.load(open('D:\machine learning projects\MULTIPLE DISEASE PREDICTION\multiple_disease/parkinsson_disease.sav','rb'))
+parkinsson_model=pickle.load(open('https://github.com/teja96764247/MULTIPLE-DISEASE-PREDICTIVE-SYSTEM./blob/main/parkinsson_disease.sav','rb'))
 
 
 
